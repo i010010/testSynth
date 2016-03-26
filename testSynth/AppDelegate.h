7553512422de66902bba3e0328934986f5849414
010010 <apple@apple-no-macbook.local>
2016-03-26 23:25:58 +0900

    Initial Commit

---
+++ testSynth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSynth
+//
+//  Created by apple on 2016/03/26.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testSynth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testSynth
+//
+//  Created by apple on 2016/03/26.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
